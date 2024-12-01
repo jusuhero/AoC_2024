0d0ed80d6dbf6d51fe5f784f844a367404467ff2
jusuhero <justin@ackermann.saarland>
2024-12-01 19:43:06 +0100

    Day 1 solution

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2024
+
+My solutions for the Advent of Code Coding Challenge of 2024 
+
+https://adventofcode.com/
---
+++ day1.ps1
@@ -0,0 +1,23 @@
+$lines = Get-Content "D:\input.txt"
+
+$left = @(); $right = @()
+
+foreach($line in $lines) {
+    $parts = $line -split '\s+'
+    $left += $parts[0]; $right += $parts[1]
+}
+
+$total_p1 = 0; $total_p2 = 0
+$left = $left | Sort-Object; $right = $right | Sort-Object 
+
+for($i = 0; $i -lt $left.Count; $i++) {
+    $total_p1 += [math]::Abs($left[$i] - $right[$i])
+}
+
+foreach($num in $left) {
+    $count = ($right | Where-Object { $_ -eq $num }).Count
+    $similarity = [int]$num * $count 
+    $total_p2 += $similarity
+}
+
+Write-Output "Lösung Part 1: $total_p1 `nLösung Part 2: $total_p2"
