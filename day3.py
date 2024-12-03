6c56a6b2448267a7d301ec8873e55514286ad69d
jusuhero <justin@ackermann.saarland>
2024-12-03 19:51:57 +0100

    improve code quality

--- day3.py
+++ day3.py
@@ -3,25 +3,22 @@ import re
 with open("inputs/day3", "r") as f:
     lines = f.read()
 
-pattern1 = r"mul\((\d+),(\d+)\)"
-pattern2 = r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)"
-matches1 = re.findall(pattern1, lines)
-matches2 = re.finditer(pattern2, lines)
-
+pattern = r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)"
+matches = list(re.finditer(pattern, lines))
 enabled = True
-loesung_p2 = 0
-for match in matches2:
-    if match.group().startswith("mul"):
-        if enabled:
-            # Extract the numbers and calculate the product
-            x, y = map(int, match.groups())
-            loesung_p2 += x * y
-    elif match.group() == "do()":
-        enabled = True  # Enable processing
-    elif match.group() == "don't()":
-        enabled = False
 
-
-loesung_p1 = sum(int(x) * int(y) for x, y in matches1)
+loesung_p1 = sum(
+    int(x) * int(y)
+    for match in matches
+    if match.group().startswith("mul")
+    for x, y in [match.groups()]
+)
+loesung_p2 = sum(
+    int(x) * int(y)
+    for match in matches
+    if (enabled := match.group() == "do()" or (enabled and match.group() != "don't()"))
+    and match.group().startswith("mul")
+    for x, y in [match.groups()]
+)
 
 print(f"Lösung Part 1: {loesung_p1}\nLösung Part 2: {loesung_p2}")
